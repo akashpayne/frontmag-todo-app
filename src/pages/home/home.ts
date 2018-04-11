b85623aa7d2b918e2f1f5d14e3418fb0875b8eb8
Marco Fernandes <marco@front.no>
2018-04-11 15:09:54 +0100

    Add todos UI and logic.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TodosProvider } from '../../providers/todos/todos';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [TodosProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos: Array<any> = [];
+  public todo: String = '';
 
+  constructor(public navCtrl: NavController, public todosProvider: TodosProvider) {
+    this.todos = this.todosProvider.list();
   }
 
+  addTodo() {
+    if (!this.todo) {
+      return;
+    }
+
+    this.todos.push({text: this.todo, complete: false});
+
+    this.todosProvider.add(this.todo);
+  }
+
+  completeTodo(todo) {
+    this.todosProvider.complete(todo);
+  }
+
+  deleteTodo(todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+
+    this.todosProvider.delete(todo);
+  }
 }
