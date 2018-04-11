e3624f11d10742a0616e2e6a7c8b1540c0e41f2f
Marco Fernandes <marco@front.no>
2018-04-11 16:56:21 +0100

    Add Firebase project settings.

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyACjzK5Edn9mwTe3pY35oy9TWWLZjhykt4',
+  authDomain: 'frontmag-todos.firebaseapp.com',
+  databaseURL: 'https://frontmag-todos.firebaseio.com',
+  projectId: 'frontmag-todos',
+  storageBucket: 'frontmag-todos.appspot.com',
+  messagingSenderId: '478702080463'
+};
